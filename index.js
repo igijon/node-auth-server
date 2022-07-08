793d0d27842ab5c4578984d5571daca021e40214
Inma Gijón <igijoninf@cifpvirgendegracia.com>
2022-07-08 14:21:25 +0200

    Crear rutas de la aplicación

--- index.js
+++ index.js
@@ -8,15 +8,8 @@ const express = require('express');
 // Crear el servicor/aplicación de express
 const app = express();
 
-
-//Ejemplo de petición GET
-app.get('/', (req, res) => {
-    res.json({
-        ok: true,
-        msg: 'Todo salió guay',
-        uid: 1234
-    });
-}); 
+//Rutas
+app.use('/api/auth', require('./routes/auth'));
 
 app.listen( 4000, () => {
     console.log(`Servidor corriendo en puerto ${4000}`);
