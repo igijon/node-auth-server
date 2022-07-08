bab6741db9da74130afa760c458b5b18cf5f9c71
Inma Gijón <igijoninf@cifpvirgendegracia.com>
2022-07-08 13:59:05 +0200

    Configuracion de express y ejemplo de get

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log('Hola desde node!');
+
+
+const express = require('express');
+
+
+
+
+// Crear el servicor/aplicación de express
+const app = express();
+
+
+//Ejemplo de petición GET
+app.get('/', (req, res) => {
+    res.json({
+        ok: true,
+        msg: 'Todo salió guay',
+        uid: 1234
+    });
+}); 
+
+app.listen( 4000, () => {
+    console.log(`Servidor corriendo en puerto ${4000}`);
+});
