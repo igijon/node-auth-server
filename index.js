33f2008e665daa54886d77961b925f83284b5a9a
Inma Gijón <igijoninf@cifpvirgendegracia.com>
2022-07-11 13:25:46 +0200

    Configuración de CORS y del body

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 
 
 const express = require('express');
+const cors = require('cors');
 
 
 
 
 // Crear el servicor/aplicación de express
 const app = express();
+
+//CORS
+app.use(cors());
+
+//Lectura y parseo del body
+app.use(express.json());
 
 //Rutas
 app.use('/api/auth', require('./routes/auth'));
