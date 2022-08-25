30f36afa69863894e5828076437fb875dfa67604
Inma Gijón <igijoninf@cifpvirgendegracia.com>
2022-08-25 11:00:41 +0200

    Validar campos: creación usuario

--- routes/auth.js
+++ routes/auth.js
@@ -5,7 +5,11 @@ const { crearUsuario, loginUsuario, revalidarToken } = require('../controllers/a
 const router = Router();
 
 //Crear un nuevo usuario
-router.post('/new', crearUsuario);
+router.post('/new', [
+    check('email', 'El email es obligatorio').isEmail(),
+    check('name', 'El nombre es obligatorio').not().isEmpty(),
+    check('password', 'La contraseña es obligatoria').isLength({min:6})
+], crearUsuario);
 
 //Login de usuario
 router.post('/', [
