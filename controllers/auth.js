e2ed86178c6e43d834abfacda0cf37c758b5569e
Inma Gijón <igijoninf@cifpvirgendegracia.com>
2022-08-25 10:32:12 +0200

    Validar campos obligatorios: login

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,5 @@
 const { response } = require('express');
+const { validationResult } = require('express-validator');
 
 const crearUsuario = (req, res = response) => {
     const { email, name, password } = req.body;
@@ -9,7 +10,17 @@ const crearUsuario = (req, res = response) => {
     });
 }
 
-const loginUsuario = (req, res) => {
+const loginUsuario = (req, res = response) => {
+    const errors = validationResult( req );
+    
+    if(!errors.isEmpty())
+    {
+        return res.status(400).json({
+            ok: false,
+            errors: errors.mapped()
+        });
+    }
+    
     const { email, password } = req.body;
     console.log(email, password);
     return res.json({
@@ -18,7 +29,7 @@ const loginUsuario = (req, res) => {
     });
 }
 
-const revalidarToken = (req, res) => {
+const revalidarToken = (req, res = response) => {
     return res.json({
         ok: true,
         msg: 'Renew'
